40ea7aca0a337fe1eb6451107431d69f88d58dfa
Jeffrey Shaver <jeff.e.shaver@gmail.com>
2017-03-02 11:09:11 -0500

    fix issue with find promise

--- utilities/find-promise.js
+++ utilities/find-promise.js
@@ -1,6 +1,6 @@
 const find = (collection, query) => {
   return new Promise((resolve, reject) => {
-    collection.find(query, (error, result) => {
+    collection.find(query).toArray((error, result) => {
       if (error) {
         reject(error)
 
